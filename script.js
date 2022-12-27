36da163a92d5a23018ba83cd76d948b1531edc1b
coolenedparrot <113797227+coolenedparrot@users.noreply.github.com>
2022-12-27 12:35:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,73 @@
-var x = document.getElementById("userSub");
-var y = document.getElementById("userProf");
-y.style.display = "none";
+var subVis = document.getElementById("userSub");
+var Profvis = document.getElementById("userProf");
+Profvis.style.display = "none";
+var userAge;
+var ageGroup;
+var userGender;
+var saveAge;
+var saveGeneder;
+
+
+function subInput () {
+userAge = document.getElementById("num").value;
+userGender = document.getElementById("profile-gender").value;
+if (userAge < 3) {
+    ageGroup = "infant"
+} else if (userAge < 13){
+    ageGroup = "child"
+} else if (userAge < 20){
+    ageGroup = "teenager"   
+} else if (userAge < 60){
+    ageGroup = "adult"
+}  else{
+    ageGroup = "old"
+}
+
+document.getElementById("profileAge").innerHTML= userAge;
+
+console.log(userAge);
+console.log(userGender);
+console.log(ageGroup);
+}
+
+
+function saveInput () {
+    userAge = document.getElementById("num").value;
+    userGender = document.getElementById("profile-gender").value;
+    document.getElementById("profileAge").innerHTML= userAge;
+    if (userAge < 3) {
+        ageGroup = "infant"
+    } else if (userAge < 13){
+        ageGroup = "child"
+    } else if (userAge < 20){
+        ageGroup = "teenager"   
+    } else if (userAge < 60){
+        ageGroup = "adult"
+    }  else{
+        ageGroup = "old"
+    }
+    
+    console.log(userAge);
+    console.log(userGender);
+    console.log(ageGroup);
+    localStorage.setItem("saveAge",userAge);
+    localStorage.setItem("saveAgeGroup",ageGroup);
+    localStorage.setItem("saveGender",userGender);
+}
+
+function storageOutput(){
+    ageGroup = localStorage.getItem("saveAge");
+    ageGroup = localStorage.getItem("saveAgeGroup");
+    userGender = localStorage.getItem("saveGender");
+    document.getElementById("profileAge").innerHTML= userAge;
+}
+
+
+
+
+
+
+
 
 let image = document.getElementById("image");
 const api_url = 'https://api.unsplash.com/photos/random?query=portrait&client_id=DSpfugB7jcZWFimvHwybAWNR4XWCkIlZeR8PXrx6u2c'
@@ -20,31 +87,18 @@ async function getImg() {
 
 }
 
-function show() {
 
-    /* Get image and change value
-    of src attribute */
-    // let image = document.getElementById("image");
-
-    image.src = "change this to the image API"
-
-
-
-
-    document.getElementById("profileName").innerHTML = "change this to the name API";
-}
-
-function screenShare() {
-    if (x.style.display === "none") {
-        x.style.display = "block";
+function switchScreen() {
+    if (subVis.style.display === "none") {
+        subVis.style.display = "block";
     } else {
-        x.style.display = "none";
+        subVis.style.display = "none";
     }
 
-    if (y.style.display === "none") {
-        y.style.display = "block";
+    if (Profvis.style.display === "none") {
+        Profvis.style.display = "block";
     } else {
-        y.style.display = "none";
+        Profvis.style.display = "none";
     }
 }
 
